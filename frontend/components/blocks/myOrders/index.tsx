8e3d8ac2ce6210eac5475dfec5db780e48a209e1
Ant017 <antika.noor98@gmail.com>
2024-02-13 20:12:04 +0600

    part2

---
+++ frontend/components/blocks/myOrders/index.tsx
@@ -0,0 +1,14 @@
+import './index.scss';
+
+const MyOrders = () => {
+    return (
+        <div className="my-orders-container">
+            <div className="my-orders-header">
+                <h3 className="my-orders-title">My Orders</h3>
+            </div>
+            <div className="my-orders-body">My orders</div>
+        </div>
+    )
+}
+
+export default MyOrders;
